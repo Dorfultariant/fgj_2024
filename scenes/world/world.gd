474900ee331f4ca738a1c435ee0d248e14b396ad
Dorfultariant <tunxero@hotmail.com>
2024-01-27 09:45:43 +0200

    paths for first level

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -6,16 +6,16 @@ var speed : int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var telebatties = telebatties_scene.instantiate() # Replace with function body.
-	$Route/RoutedObjects/Telebatties.add_child(telebatties)
+	$Route/Teletubbies.add_child(telebatties)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Route/RoutedObjects.progress += speed * delta
+	$Route/Teletubbies.progress += speed * delta
 
 
 
 func _on_telebatties_timer_timeout():
 	var telebatties = telebatties_scene.instantiate() # Replace with function body.
-	$Route/RoutedObjects/Telebatties.add_child(telebatties)
+	$Route/Teletubbies.add_child(telebatties)
 	
