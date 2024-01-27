042f8b8bf1d58c98fccc376bd6ad74e7af270e42
Dorfultariant <tunxero@hotmail.com>
2024-01-27 10:40:54 +0200

    Enum for globals and Telebatties queue

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -12,11 +12,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Route/Teletubbies.progress += speed * delta
-
+	if Globals.telebatties_queue:
+		for tub in Globals.telebatties_queue:
+			$Route/Teletubbies.add_child(tub)
+		Globals.telebatties_queue.empty
 
 
 func _on_telebatties_timer_timeout():
 	var telebatties = telebatties_scene.instantiate() # Replace with function body.
-	$Route/Teletubbies.add_child(telebatties)
-	
+	Globals.telebatties_queue.append(telebatties)
+
+
+func _on_tele_bat_spawn_timer_timeout():
+	var newFollowPath = PathFollow2D.new()
+	newFollowPath.add_child(Globals.telebatties_queue.pop_at(0))
+	$Route.add_child(newFollowPath)
+
+
+func _on_finnish_line_tele_bat_got_in(body):
+	pass # Replace with function body.
