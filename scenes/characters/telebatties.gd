10b316a3933031f8cdf1b2eccba318528a18e487
Dorfultariant <tunxero@hotmail.com>
2024-01-27 08:56:26 +0200

    baselevel

--- scenes/characters/telebatties.gd
+++ scenes/characters/telebatties.gd
@@ -2,3 +2,13 @@ extends CharacterBody2D
 class_name TELE
 
 
+const type_list = ["red","lila","blue","green","pink","light"]
+var type
+var health : int = 0
+
+func _ready():
+	type = "red"
+	health = 100
+
+
+
--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -1,11 +1,21 @@
 extends Node2D
 class_name WORLD
+var telebatties_scene: PackedScene = preload("res://scenes/characters/telebatties.tscn")
 
+var speed : int = 200
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var telebatties = telebatties_scene.instantiate() # Replace with function body.
+	$Route/RoutedObjects/Telebatties.add_child(telebatties)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$Route/RoutedObjects.progress += speed * delta
+	
+
+
+func _on_telebatties_timer_timeout():
+	var telebatties = telebatties_scene.instantiate() # Replace with function body.
+	$Route/RoutedObjects/Telebatties.add_child(telebatties)
+	
