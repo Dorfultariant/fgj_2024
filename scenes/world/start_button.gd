69f79d2f63f19d8d7237b245daf0cf5722f7ffa5
Lauri-Heiskanen <lauri.heiskanen.6@gmail.com>
2024-01-27 20:06:33 +0200

    fixed main_menu and made lose_screen

--- scenes/world/start_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-var simultanious_scene = preload("res://scenes/world/world.tscn").instantiate()
-
-
-func _on_pressed():
-	get_tree().root.add_child(simultanious_scene)
-	queue_free()
