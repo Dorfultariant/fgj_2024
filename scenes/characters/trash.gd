9704719acef00d35ce3ca1f82ba7bb46148c8048
Dorfultariant <tunxero@hotmail.com>
2024-01-28 01:09:00 +0200

    Some global madness

--- scenes/characters/trash.gd
+++ scenes/characters/trash.gd
@@ -4,20 +4,18 @@ extends CharacterBody2D
 const SPEED = 800.0
 const ROTATE_SPEED = 1000
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var damage
+var damage = 50
 var pierce
 var targetPosition
+
 @export var speed: int = 2500
 var direction: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$SelfDestructTimer.start()
-	pass
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	position += direction * SPEED * delta
 	rotate(deg_to_rad(ROTATE_SPEED)*delta)
@@ -29,8 +27,6 @@ func _on_self_destruct_timer_timeout():
 
 
 func _on_area_2d_body_entered(body):
-	#print(get_tree().get_root())
-	#if "Telebatties" in body.name:
-		#get_tree().get_root().get_node("/root/scenes/")
-		#body.hit()
+	if "Telebatties" in body.name:
+		body.hit(damage)
 	queue_free() # Replace with function body.
